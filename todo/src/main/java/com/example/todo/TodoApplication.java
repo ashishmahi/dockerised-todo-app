2312fa78047ab59cd3ded6c44ac7e612bb2e43e6
ashishm <ashishmahi46@gmail.com>
2019-02-09 13:02:06 +0530

    add post action handler for todo

--- todo/src/main/java/com/example/todo/TodoApplication.java
+++ todo/src/main/java/com/example/todo/TodoApplication.java
@@ -1,17 +1,20 @@
 package com.example.todo;
 
-import org.apache.tomcat.util.json.JSONParser;
-import org.json.JSONArray;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+
 
 @SpringBootApplication
 @RestController
@@ -25,18 +28,30 @@ public class TodoApplication {
     @RequestMapping("/todos")
     public String getTodo() {
         try {
-            String cwd = System.getProperty("user.dir");
-            System.out.println("Current working directory : " + cwd);
-            byte[] contents = Files.readAllBytes(Paths.get("todos.json"));
-            return new String(contents, "UTF-8");
+            byte[] contents = Files.readAllBytes(Paths.get("todos.txt"));
+            String todos = new String(contents, "UTF-8");
+            return todos;
         } catch (IOException e) {
             e.printStackTrace();
         }
         return null;
     }
 
-    @RequestMapping(value = "/todos", method = RequestMethod.POST)
-    public void postTodo(@RequestBody Todo todo) {
+    @PostMapping(value = "/todos")
+    public ResponseEntity<String> postTodo(@RequestBody Todo todo) {
+        try {
+            File file = new File("todos.txt");
+            FileWriter fr = new FileWriter(file, true);
+            fr.append(todo.toString());
+            fr.close();
+            return new ResponseEntity<>("result successful result",
+                    HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>("nigh ithan",
+                    HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
     }
 
 
