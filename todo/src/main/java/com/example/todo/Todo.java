7b9e4d355147a723ca99fe0e621834322737f4f3
ashishm <ashishmahi46@gmail.com>
2019-02-09 13:01:10 +0530

    override toString method in Todo

--- todo/src/main/java/com/example/todo/Todo.java
+++ todo/src/main/java/com/example/todo/Todo.java
@@ -1,5 +1,13 @@
 package com.example.todo;
 
+
 public class Todo {
     public String details;
+
+    @Override
+    public String toString() {
+        return "{" +
+                "details='" + details + '\'' +
+                '}';
+    }
 }
